5cb3bf68f8ef1f2adf00561e94e21b8be8a6056a
BigWhiteDog <legend.z@foxmail.com>
2022-03-29 16:02:27 +0800

    add an example for nemu singnal

---
+++ apps/new_before_workload/new_before_workload.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void nemu_signal(int op)
+{
+    asm volatile("mv a0, %0; .word 0x0000006b;"::"r"(op));
+}
+
+
+int main(int argc, char const *argv[])
+{
+    nemu_signal(257);
+    return 0;
+}
