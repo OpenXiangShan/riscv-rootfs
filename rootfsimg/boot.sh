7ad0a9e9865d956b0a18883e397e21d16d16b43b
XiChen <chenxi171@mails.ucas.ac.cn>
2022-11-21 22:39:50 +0800

    update boot.sh for 248

--- rootfsimg/boot.sh
+++ rootfsimg/boot.sh
@@ -2,12 +2,14 @@
 
 # Config IP address through DHCP
 # udhcpc
-ifconfig eth0 172.28.11.10 netmask 255.255.255.0
-ip route add default via 172.28.11.1
+# ifconfig eth0 172.28.2.10 netmask 255.255.255.0
+# ip route add default via 172.28.2.1
+ifconfig eth0 192.168.1.2 netmask 255.255.255.0
 
 # Mount NFS
 # mount -t nfs -o defaults,nolock 172.28.9.102:/home/user01/xs_nfs /mnt
-mount -t nfs -o defaults,nolock 172.28.11.119:/home/cxhpc/nanhu-nfs/nfs /mnt
+# mount -t nfs -o defaults,nolock 172.28.11.119:/home/cxhpc/nanhu-nfs/nfs /mnt
+mount -t nfs -o defaults,nolock 192.168.1.200:/home/user01/nanhu-nfs /mnt
 
 # Boot on NFS
 exec switch_root /mnt /sbin/init
