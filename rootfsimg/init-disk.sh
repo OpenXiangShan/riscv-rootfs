28dcbcf6ff87ea8dcb2e64b62b497e16cf19bb15
Wonicon <conciselove@outlook.com>
2025-03-31 14:06:39 +0800

    checkpoint: support large workload from disk (#17)
    Add a new initramfs-disk.txt which uses init-disk.sh as /init
    to automatically mount virtio disk, copy env into ram disk,
    and chroot into disk env in mem.

---
+++ rootfsimg/init-disk.sh
@@ -0,0 +1,35 @@
+#!/bin/busybox sh
+
+set -e
+
+echo "Init started with PID: $$"
+
+if [ "$$" -ne 1 ]; then
+  echo "Error: /init must be PID 1"
+  exit 1
+fi
+
+/bin/busybox --install -s
+
+mount -t proc proc /proc
+mount -t sysfs sysfs /sys
+mount -t devtmpfs devtmpfs /dev
+mkdir -p /run
+mount -t tmpfs tmpfs /run
+
+mkdir /ram_root
+mount -t tmpfs -o size=6G tmpfs /ram_root
+
+echo "Move disk fs into memory"
+mount /dev/vda /mnt 
+cp /mnt/* /ram_root/
+umount /mnt
+
+echo "Chroot into disk fs in memory"
+
+mount --bind /dev /ram_root/dev
+mount -t sysfs sysfs /ram_root/sys
+mount -t sysfs proc /ram_root/proc
+mount -t tmpfs tmpfs /ram_root/run
+
+chroot /ram_root /bin/bash
